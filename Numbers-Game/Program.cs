a6d496e4c266776cd4a18f216ae03a983c83d58c
mistidinzy <mistidinzy@gmail.com>
2021-10-04 23:22:49 -0500

    create sln file

---
+++ Numbers-Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Numbers_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
