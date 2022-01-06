19ad9a2f16e49fdb2039f02ab1c5cdca2719cce9
mistidinzy <mistidinzy@gmail.com>
2022-01-06 15:03:18 -0600

    refactoring console strings

--- Numbers-Game/Program.cs
+++ Numbers-Game/Program.cs
@@ -13,7 +13,7 @@ namespace Numbers_Game
             //Generic Exception
             catch (Exception ex)
             {
-                Console.WriteLine("I'm sorry. Something went wrong here." + ex.Message);
+                Console.WriteLine("I'm sorry. Something went wrong here. " + ex.Message);
             }
             //include a finally that tells us that the program is completed.
             finally
@@ -25,16 +25,18 @@ namespace Numbers_Game
         static void StartSequence()
         {
             //prompt the user to “Enter a number greater than zero”
-            Console.WriteLine("Hello! Give me a number greater than zero, please.");
+            Console.WriteLine("Hello! Give me a number between one and ten, please.");
 
             string userInput = Console.ReadLine();
             Console.WriteLine($"You chose { userInput}.");
+            Console.WriteLine(" ");
 
             try
             {
                 //convert the user’s input to an integer
                 int userInputNumber = Convert.ToInt32(userInput);
                 Console.WriteLine($"The array will have...{userInput} numbers in it.");
+                Console.WriteLine(" ");
 
                 //Instantiate a new integer array that is the size the user just inputted.
                 int[] newArray = new int[userInputNumber];
@@ -44,17 +46,21 @@ namespace Numbers_Game
 
                 //Capture the sum by calling the GetSum method
                 int captureSum = GetSum(populatedArray);
-                Console.WriteLine($"The sum of all of the numbers in your array is... {captureSum}.");
+                Console.WriteLine($"The sum of all of the numbers in your array is...{captureSum}.");
+                Console.WriteLine(" ");
 
                 //Capture the product by calling the GetProduct method
                 int captureProduct = GetProduct(populatedArray, captureSum);
-                Console.WriteLine($"The product is... {captureProduct}.");
+                Console.WriteLine($"The product is...{captureProduct}.");
+                Console.WriteLine(" ");
 
                 //Capture the quotient by calling the GetQuotient method
-                //int captureQuotient = GetQuotient();
+                decimal captureQuotient = GetQuotient(captureProduct);
 
                 //output to the console the details of all these values
-                Console.WriteLine("");
+                Console.WriteLine($"The sum was {captureSum}. The product was {captureProduct}. The quotient was {captureQuotient}.");
+                Console.WriteLine(" ");
+                Console.WriteLine("Thanks for playing!");
 
             }
             catch (FormatException exc)
@@ -81,6 +87,8 @@ namespace Numbers_Game
                 numArr[i] = toNumber;
 
                 Console.WriteLine("The array so far is: [{0}]", string.Join(", ", numArr));
+                Console.WriteLine(" ");
+
             }
 
             //return the populated array
@@ -106,19 +114,48 @@ namespace Numbers_Game
         {
             //Ask the user to select a number between 1 & the length of the integer array.
             Console.WriteLine($"Please choose a number between 1 and { intArr.Length - 1}.");
+            Console.WriteLine(" ");
 
             string userInput = Console.ReadLine();
+            Console.WriteLine(" ");
+
             Console.WriteLine("You chose " + userInput + ". Whichever number is in that array position will be what we multiply against the sum.");
+            Console.WriteLine(" ");
 
             Console.WriteLine("Checking the value...");
+            Console.WriteLine(" ");
 
             int indexPosition = Convert.ToInt32(userInput);
+            Console.WriteLine(intArr[indexPosition]);
+
             Console.WriteLine("The value in that position is " + intArr[indexPosition] + ".");
+            Console.WriteLine(" ");
+
 
             Console.WriteLine("Calculating the product...");
+            Console.WriteLine(" ");
 
             int product = sum * indexPosition;            
             return product;
+        }
+
+        static decimal GetQuotient(int product)
+        {
+            Console.WriteLine($"Now we're going to divide the product. What number should we divide it by?");
+            Console.WriteLine(" ");
+
+            string userInput = Console.ReadLine();
+            Console.WriteLine($"Okay, the dividend will be {userInput}.");
+            Console.WriteLine(" ");
+
+            int dividend = Convert.ToInt32(userInput);
+
+            decimal quotient = decimal.Divide(product, dividend);
+
+            Console.WriteLine($"The quotient is {quotient}!");
+            Console.WriteLine(" ");
+
+            return quotient;
         }
     }
 }
